32cacf63774590546bd3b81b340f303dbf6a4ebd
JeffreyMJohnson <jefjohms@gmail.com>
2015-08-15 18:03:41 -0700

    re-factored code and added unit tests.  Can now switch between data sources easily and future sources can be added as well.

---
+++ GoogleDataSource.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.GData.Client;
+using Google.GData.Spreadsheets;
+
+namespace ScriptXMLConvert
+{
+    public class GoogleDataSource : IScriptDataProvider
+    {
+
+        //OAuth config
+        private string CLIENT_ID = "898242977449-gdhq44lj4h22jgv2gougnaktg6i482p9.apps.googleusercontent.com";
+        private string CLIENT_SECRET = "kiR_ogLwko8r_HviWpSWyj2p";
+        private string SCOPE = "https://spreadsheets.google.com/feeds";
+        private string REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob";
+
+        private List<SheetRow> _rows = new List<SheetRow>();
+
+        public GoogleDataSource()
+        {
+            ListFeed feedList = GetListFeed();
+            LoadRows(feedList);
+        }
+
+        public SheetRow[] GetRows()
+        {
+            return _rows.ToArray();
+        }
+
+        private void LoadRows(ListFeed listFeed)
+        {
+            foreach (ListEntry row in listFeed.Entries)
+            {
+                //no blank rows with google data or data will stop
+                _rows.Add(new SheetRow
+                {
+                    Scene = GetCellText(row, ColumnHeader.Scene),
+                    Moment = GetCellText(row, ColumnHeader.Moment),
+                    Line = GetCellText(row, ColumnHeader.Line),
+                    Duration = GetCellText(row, ColumnHeader.Duration),
+                    Location = GetCellText(row, ColumnHeader.Location),
+                    SFX = GetCellText(row, ColumnHeader.SFX)
+                });
+                if (GetCellText(row, ColumnHeader.Scene) == "SCRIPT TOTAL DURATION")
+                {
+                    //all done last line
+                    break;
+                }
+            }
+           
+        }
+
+        private string GetCellText(ListEntry row, ColumnHeader column)
+        {
+            if(null == row.Elements[(int)column])
+            {
+                return "";
+            }
+            return row.Elements[(int)column].Value;
+        }
+
+
+        private ListFeed GetListFeed()
+        {
+            //    //setup OAuth object
+            OAuth2Parameters parameters = new OAuth2Parameters();
+            parameters.ClientId = CLIENT_ID;
+            parameters.ClientSecret = CLIENT_SECRET;
+            parameters.RedirectUri = REDIRECT_URI;
+            parameters.Scope = SCOPE;
+
+            //get auth URL
+            string authorizationUrl = OAuthUtil.CreateOAuth2AuthorizationUrl(parameters);
+            Console.WriteLine(authorizationUrl);
+
+            //occasionally the clipboard decides to not work throwing this exception.  I copy to the clipboard for convenience, so if
+            //doesn't work, user can select and copy the URL manually.
+            try
+            {
+                System.Windows.Forms.Clipboard.Clear();
+                System.Windows.Forms.Clipboard.SetText(authorizationUrl);
+                Console.WriteLine("*****URL Copied to clipboard.*****");
+            }
+            catch (System.Runtime.InteropServices.ExternalException e)
+            {
+                Console.WriteLine("*****There was a problem copying to the clipboard, so copy the URL manually.*****");
+            }
+
+            Console.WriteLine("Please visit the URL above to authorize your OAuth request token.  Once that is complete, type in your access code to continue...");
+
+            parameters.AccessCode = Console.ReadLine();
+
+            try
+            {
+                OAuthUtil.GetAccessToken(parameters);
+            }
+            catch (System.Net.WebException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Did you copy/paste the given code exactly?");
+                throw e;
+            }
+
+            string accessToken = parameters.AccessToken;
+            Console.WriteLine("OAuth Access Token: " + accessToken);
+
+            GOAuth2RequestFactory requestFactory = new GOAuth2RequestFactory(null, "MySpreadsheetIntegration-v1", parameters);
+            SpreadsheetsService service = new SpreadsheetsService("MySpreadsheetIntegration-v1");
+            service.RequestFactory = requestFactory;
+
+            // Instantiate a SpreadsheetQuery object to retrieve spreadsheets.
+            SpreadsheetQuery query = new SpreadsheetQuery();
+
+            // Make a request to the API and get all spreadsheets.
+            SpreadsheetFeed feed = service.Query(query);
+
+            if (feed.Entries.Count == 0)
+            {
+                Console.WriteLine("No Spreadsheets found on Google Drive.");
+                throw new Exception();
+            }
+
+            SpreadsheetEntry spreadsheet = null;
+            foreach (SpreadsheetEntry entry in feed.Entries)
+            {
+                if (entry.Title.Text == "SCENE BREAKDOWN - KANSAS")
+                {
+                    spreadsheet = (SpreadsheetEntry)entry;
+                }
+            }
+
+            if (null == spreadsheet)
+            {
+                Console.WriteLine("Could not find spreadsheet 'SCENE BREAKDOWN - KANSAS' on Google Drive.");
+                Console.WriteLine("Did you login with account that has access?");
+                throw new Exception();
+            }
+            else
+            {
+                Console.WriteLine("Spreadsheet 'SCENE BREAKDOWN - KANSAS' found on Google Drive.");
+            }
+
+            WorksheetFeed wsFeed = spreadsheet.Worksheets;
+            WorksheetEntry worksheet =  (WorksheetEntry)wsFeed.Entries[0];
+            // Define the URL to request the list feed of the worksheet.
+            AtomLink listFeedLink = worksheet.Links.FindService(GDataSpreadsheetsNameTable.ListRel, null);
+
+            // Fetch the list feed of the worksheet.
+            ListQuery listQuery = new ListQuery(listFeedLink.HRef.ToString());
+            ListFeed listFeed = service.Query(listQuery);
+            if (listFeed.Entries.Count < 1)
+            {
+                Console.WriteLine("No rows returned with the data.\nDid you sign on with account that has access to spreadsheet on Google Drive?");
+            }
+
+            return listFeed;
+        }
+
+    }
+}
---
+++ Unit_Tests/ScriptXMLConverter_unit_test/ScriptTests.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScriptXMLConvert;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ScriptXMLConverter_unit_test
+{
+    [TestClass()]
+    public class ScriptTests
+    {
+        const string TEST_DATA_PATH_1 = @"..\..\Data\TestData_1.xlsx";
+
+        [TestInitialize]
+        public void TestInit()
+        {
+
+        }
+
+        [TestMethod()]
+        public void ScriptTest()
+        {
+            Script script = new Script();
+
+            Assert.AreEqual(0, script.Acts.Length);
+            Assert.IsNull(script.TotalTime);
+        }
+
+        [TestMethod()]
+        public void ScriptTest2()
+        {
+            XLSXDataSource data = new XLSXDataSource(TEST_DATA_PATH_1);
+            SheetRow[] rows = data.GetRows();
+            Script script = new Script(rows);
+
+            Assert.AreEqual("01:30:00", script.TotalTime);
+            Assert.AreEqual(2, script.Acts.Length);
+            Assert.AreEqual(3, script.Acts[0].Scenes.Length);
+            Assert.AreEqual(3, script.Acts[1].Scenes.Length);
+
+            Assert.AreEqual(12, script.Acts[0].Scenes[0].Moments.Length);
+            Assert.AreEqual("FIRST SET : INTRO", script.Acts[0].Scenes[0].Moments[0].Title);
+            Assert.AreEqual("", script.Acts[0].Scenes[0].Moments[0].Duration);
+
+            Moment momentUnderTest = script.Acts[1].Scenes[1].Moments[6];
+            Assert.AreEqual("Out of sex scene", momentUnderTest.Title);
+            Assert.AreEqual("Guess you're not in Kansas any more", momentUnderTest.Line);
+            Assert.AreEqual("", momentUnderTest.Duration);
+            Assert.AreEqual("", momentUnderTest.Location);
+            Assert.AreEqual("", momentUnderTest.SFX);
+
+        }
+
+        [TestMethod()]
+        public void AddAttribute()
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlElement root = doc.CreateElement("root");
+            doc.AppendChild(root);
+            Script script = new Script();
+            PrivateObject priv_script = new PrivateObject(script);
+
+            priv_script.Invoke("AddAttribute", doc, root, "rootAttribute", "attribute value");
+
+            Assert.AreEqual(1, doc.ChildNodes.Count);
+            Assert.AreEqual(1, root.Attributes.Count);
+            Assert.AreEqual("rootAttribute", root.Attributes.GetNamedItem("rootAttribute").Name);
+            Assert.AreEqual("attribute value", doc.FirstChild.Attributes.GetNamedItem("rootAttribute").Value);
+        }
+
+        [TestMethod()]
+        public void GetXML()
+        {
+            XLSXDataSource data = new XLSXDataSource(TEST_DATA_PATH_1);
+            SheetRow[] rows = data.GetRows();
+            Script script = new Script(rows);
+            XmlDocument doc = script.GetXML();
+
+            Assert.AreEqual(2, doc.SelectNodes("script/act").Count);
+            Assert.AreEqual(3, doc.SelectNodes("script/act[@number='1']/scene").Count);
+            Assert.AreEqual(4, doc.SelectNodes("script/act[@number='2']/scene[@number='3']/moment").Count);
+            Assert.AreEqual("We will need to shut down her ", doc.SelectSingleNode("script /act[@number='2']/scene[@number='3']/moment[@title='Planning Dialogue, Leo joins']/@line").Value);
+
+        }
+    }
+}
